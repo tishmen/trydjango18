226f43fdfd2b831def48154ef7c3fe1f9163ba6c
tishmen <topuzov.work@gmail.com>
2016-08-15 14:34:50 +0200

    added admin for model sign up

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import SignUp
+
 # Register your models here.
+
+
+@admin.register(SignUp)
+class SignUpAdmin(admin.ModelAdmin):
+
+    list_display = ['email', 'added', 'changed']
