623696de5bf2a46ae80a82ffa97e65e8de5989cd
tishmen <topuzov.work@gmail.com>
2016-08-16 16:28:45 +0200

    changes in signuup view and template

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 
+from .forms import SignUpForm
+
 # Create your views here.
 
 
 def home(request):
     title = 'Welcome!'
     if request.user.is_authenticated():
-        title = 'Welcome! {}'.format(request.user)
-    context = {'title': title}
+        title += ' {}'.format(request.user)
+    form = SignUpForm(request.POST or None)
+    context = {'title': title, 'form': form}
+    if form.is_valid():
+        obj = form.save(commit=False)
+        if not obj.full_name:
+            obj.full_name = 'tishmen'
+        obj.save()
+        context = {'title': 'Thank you'}
     return render(request, 'home.html', context)
