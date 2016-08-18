985001eed3e87b2ed7346492d5dffb8d34123fe9
tishmen <topuzov.work@gmail.com>
2016-08-18 11:08:15 +0200

    refactored home view

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -8,17 +8,12 @@ from .forms import SignUpForm, ContactForm
 
 
 def home(request):
-    title = 'Hello'
-    if request.user.is_authenticated():
-        title += ' {}'.format(request.user)
     form = SignUpForm(request.POST or None)
-    context = {'title': title, 'form': form}
     if form.is_valid():
         obj = form.save(commit=False)
-        if not obj.full_name:
-            obj.full_name = 'default'
+        obj.full_name = obj.full_name or request.user or 'AnonymousUser'
         obj.save()
-    return render(request, 'home.html', context)
+    return render(request, 'home.html', {'title': 'Welcome', 'form': form})
 
 
 def contact(request):
